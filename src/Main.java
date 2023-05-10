1b8410740dc837b45f6d6cdb374f089a9b0ecfae
Orji_TheDev <117165108+orjithedev@users.noreply.github.com>
2023-05-10 21:40:07 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,37 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Something went wrong");
         }
+        public static void main(String[] args) {
+        Energy energy = new Energy();
+       
+
+        Scanner input = new Scanner(System.in);
+        try {
+            float force= input.nextFloat();
+            float distance = input.nextFloat();
+         System.out.println(Workdone(force, distance));
+        } catch (InputMismatchException ime) {
+            System.out.println("Enter a valid number");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        
+    }
+     public static void main(String[] args) {
+        Energy energy = new Energy();
+       
+
+        Scanner input = new Scanner(System.in);
+        try {
+            float mass = input.nextFloat();
+            float acceleration = input.nextFloat();
+
+            System.out.println(Force(mass, acceleration));
+        } catch (InputMismatchException ime) {
+            System.out.println("Enter a valid number");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
     }
 
 
