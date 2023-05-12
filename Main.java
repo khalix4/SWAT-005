6b51fa85450f6ce261b4612f6b436866ee61c5b4
kyangchat <tryphena.john@binghamuni.edu.ng>
2023-05-12 12:54:01 +0100

    my changes

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
        PhysicsUI myObj = new PhysicsUI();
        myObj.mainUI();
+       myObj.main3UI();
 //       myObj.recBtn();
     }
 }
--- PhysicsUI.java
+++ PhysicsUI.java
@@ -9,17 +9,17 @@ public  class PhysicsUI {
     JFrame frame = new JFrame("Window");
 
     JButton speed = new JButton("SPEED");
-    //JButton potentialEnergy = new JButton("POTENTIAL ENERGY");
-
-    //add more button
-
-
     JTextField distance = new JTextField("Enter Distance");
     JTextField time = new JTextField("Enter Time");
+
+    JButton force = new JButton("FORCE");
+    JTextField mass = new JTextField("Enter MASS");
+    JTextField acceleration = new JTextField("Enter Acceleration");
 
     JButton calculate = new JButton("calculate");
 
     public void mainUI() {
+        frame.add(speed);
         frame.add(speed);
         frame.setSize(200, 200);
         frame.setLayout(new GridLayout(5, 1));
@@ -31,6 +31,7 @@ public  class PhysicsUI {
                 calcspeed();
             }
         });
+
 
 
     }
@@ -67,6 +68,65 @@ public  class PhysicsUI {
             JOptionPane.showMessageDialog(null, "error");
         }
     }
+
+    public void main3UI() {
+        frame.add(force);
+        frame.add(force);
+        frame.setSize(200, 200);
+        frame.setLayout(new GridLayout(5, 1));
+        frame.setVisible(true);
+
+        force.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calcforce();
+            }
+        });
+
+
+
+    }
+
+
+    JFrame frame3 = new JFrame("Window");
+    Energy geo3 = new Energy();
+
+    public void calcforce() {
+        frame1.setSize(200, 200);
+        frame1.setLayout(new GridLayout(5, 1));
+        frame1.setVisible(true);
+        frame1.add(mass);
+        frame1.add(acceleration);
+        frame1.add(calculate);
+
+        calculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculate();
+            }
+        });
+
+
+    }
+
+    public void calculate2() {
+
+        try {
+
+            float force = geo.force(Float.parseFloat(mass.getText()), Float.parseFloat(acceleration.getText()));
+            JOptionPane.showMessageDialog(null, force);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "error");
+        }
+    }
+
+
+
 }
+
+
+
+
+
 
 
